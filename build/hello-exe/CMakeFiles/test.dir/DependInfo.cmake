41152c215c78de7736f4a6037987d87663c05f9e
K. Jetnipit <jetnipit54@gmail.com>
2021-06-03 14:36:29 +0700

    add cmake build

---
+++ build/hello-exe/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/user/ProjeXt/pok-simulator/src/simulator/hello-exe/main.cpp" "hello-exe/CMakeFiles/test.dir/main.cpp.obj" "gcc" "hello-exe/CMakeFiles/test.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/user/ProjeXt/pok-simulator/build/say-hello/CMakeFiles/say-hello.dir/DependInfo.cmake"
+  "C:/Users/user/ProjeXt/pok-simulator/build/calculator/CMakeFiles/calculator.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/say-hello/CMakeFiles/say-hello.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for say-hello.
+# This may be replaced when dependencies are built.
