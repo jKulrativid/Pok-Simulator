e1753a639abe0c1e68595f92589d4710ce60beab
K. Jetnipit <jetnipit54@gmail.com>
2021-06-05 14:40:29 +0700

    add player class in base

---
+++ simulation/src/base/player.hpp
@@ -0,0 +1,26 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <iostream>
+#include <vector>
+
+#include "card.hpp"
+
+class Player{
+    private:
+        std::string name;
+        std::vector<Card> hand;
+    
+    public:
+        Player();
+        Player(std::string new_name);
+        ~Player();
+
+        // GETTER && SETTER //
+        std::string get_name();
+        std::vector<Card> get_hand();
+        void set_name(std::string new_name);
+
+};
+
+#endif
--- simulation/src/main/main.cpp
+++ simulation/src/main/main.cpp
@@ -5,13 +5,6 @@
 #include <deckfactory.hpp>
 
 int main(){
-    DeckFactory factory;
-    Deck new_deck = factory.get_new_deck(1);
-    Card card = new_deck.pick_card();
-    card = new_deck.pick_card();
-    std::cout << card.get_rank() << card.get_suit() << "\n";
-    new_deck.shuffle();
-    card = new_deck.pick_card();
-    std::cout << card.get_rank() << card.get_suit() << "\n";
+    std::cout << "Still Work";
 
 }
