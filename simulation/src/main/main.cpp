310b7b1190d719cfe52230ea02224b2f8d2f5053
K. Jetnipit <jetnipit54@gmail.com>
2021-06-09 13:57:00 +0700

    start implementing simulator after finishing base

--- simulation/src/main/main.cpp
+++ simulation/src/main/main.cpp
@@ -1,37 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <unordered_map>
 
 #include <deck.hpp>
 #include <player.hpp>
 #include <deckfactory.hpp>
 
-// Put Test HERE !!!!
-void runtest(){
-    std::cout << "<<<<<<<<<<<<<<< RUN TEST >>>>>>>>>>>>>>>\n";
-    DeckFactory* df = new DeckFactory();
-    Deck* deck = df->get_new_deck(1);
-    Dealer* dealer = new Dealer("Mr.Wang");
-    Player* player = new Player("Richard");
-    dealer->set_deck(deck);
-    dealer->formal_shuffle();
-    Card* testcard1;
-    Card* testcard2;
-    dealer->hand_out_card(player, 1);
-    dealer->hand_out_card(player, 1);
-    std::cout << player->get_score();
+void init_seed_rand(){
+    std::srand(std::time(NULL));
 
 }
 
 void init_program(){
-    std::srand(std::time(NULL));
+    init_seed_rand();
 
 }
 
 int main(){
     init_program();
-    std::cout << "main Work\n";
-    for (int i=0; i<5; i++)
-        runtest();
 
 }
